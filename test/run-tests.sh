78563679b7b3d0a795334713c2b49d6c2e9e5778
blex <balabaj.a@yandex.ru>
2024-06-23 16:05:19 +0300

    Add test/26-rec-bin.kum

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -138,6 +138,8 @@ let tot+=$?
 # I do not want to spend time on this - just add ;
 do_test $1 $2 "25-rec-hanoi"
 let tot+=$?
+do_test $1 $2 "26-rec-bin"
+let tot+=$?
 do_test $1 $2 "28-rec-nod"
 let tot+=$?
 do_test_1 $1 $2 "dev-loop" "20"
